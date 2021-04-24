9545ed2346df843893bb0dce3549b8ab7fb57364
RahulAnand442001 <sahurahul442001@gmail.com>
2021-04-24 21:10:29 +0530

    added custom form components and auth pages

---
+++ src/pages/auth.js/auth.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SignIn from "../../components/signin/signin.component";
+import "./auth.styles.scss";
+
+const SignInSignUpPage = () => (
+  <div className="sign-in-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInSignUpPage;
