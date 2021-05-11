e37c3540cd5c57db47b6379fdc8375d3379aa93b
RahulAnand442001 <sahurahul442001@gmail.com>
2021-05-11 21:14:00 +0530

    opened data keys : will close on further development

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,14 +3,16 @@ import StripeCheckout from "react-stripe-checkout";
 
 const StripeCheckoutButton = ({ price }) => {
 	const priceForStripe = price * 100;
-	const publishableKey = `${process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}`;
+	// const publishableKey = `${process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}`;
+	const publishableKey =
+		"pk_test_51IptsGSFbSKAxxKb3BcrDCnFsweZr2EHZoCzBBW9jJYlXULVIb15Enh4ttXJRgZUY9YwZ6DCuuiriz8VREbh32MB00HX60thW8";
 	const onToken = (token) => {
 		alert("Payment successfull");
 	};
 	return (
 		<StripeCheckout
 			label="Pay Now"
-			name="GCROWN Clothing Ltd."
+			name="Crownion Clothings Ltd."
 			billingAddress
 			shippingAddress
 			description={`Your total is $${price}`}
