9f544badf9e2b6b61800dbf29f4d446a911a8a6c
RahulAnand442001 <sahurahul442001@gmail.com>
2021-06-12 19:12:04 +0530

    introduced react-lazy inside application along with spinner component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
@@ -7,16 +7,19 @@ import { createStructuredSelector } from "reselect";
 // import "./App.css";
 import { GlobalStyle } from "./global.styles";
 
-// curstom components (pages)
-import Header from "./components/header/header.component";
-import SignInSignUpPage from "./pages/auth.js/auth.component";
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
-import CheckoutPage from "./pages/checkout/checkout.component";
-
 // redux components
 import { selectCurrentUser } from "./redux/user/user.selectors";
 import { checkUserSession } from "./redux/user/user.actions";
+
+// components
+import Spinner from "./components/loading-spinner/spinner.componen";
+
+// curstom components (pages)
+import Header from "./components/header/header.component";
+const SignInSignUpPage = lazy(() => import("./pages/auth.js/auth.component"));
+const ShopPage = lazy(() => import("./pages/shop/shop.component"));
+const CheckoutPage = lazy(() => import("./pages/checkout/checkout.component"));
+const HomePage = lazy(() => import("./pages/homepage/homepage.component"));
 
 // react class component
 const App = ({ checkUserSession, currentUser }) => {
@@ -29,16 +32,18 @@ const App = ({ checkUserSession, currentUser }) => {
 			<GlobalStyle />
 			<Header />
 			<Switch>
-				<Route exact path="/" component={HomePage} />
-				<Route path="/shop" component={ShopPage} />
-				<Route exact path="/checkout" component={CheckoutPage} />
-				<Route
-					exact
-					path="/signin"
-					render={() =>
-						currentUser ? <Redirect to="/" /> : <SignInSignUpPage />
-					}
-				/>
+				<Suspense fallback={<Spinner />}>
+					<Route exact path="/" component={HomePage} />
+					<Route path="/shop" component={ShopPage} />
+					<Route exact path="/checkout" component={CheckoutPage} />
+					<Route
+						exact
+						path="/signin"
+						render={() =>
+							currentUser ? <Redirect to="/" /> : <SignInSignUpPage />
+						}
+					/>
+				</Suspense>
 			</Switch>
 		</>
 	);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { connect } from "react-redux";
 import { Route } from "react-router-dom";
+import Spinner from "../../components/loading-spinner/spinner.componen";
 
 // redux components
 import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 
 // custom components
-import CollectionOverviewContainer from "../../components/collections-overview/collections-overview.container";
-import CollectionsPageContainer from "../collection/collection.container";
+const CollectionOverviewContainer = lazy(() =>
+	import(
+		"../../components/collections-overview/collections-overview.container"
+	),
+);
+const CollectionsPageContainer = lazy(() =>
+	import("../collection/collection.container"),
+);
 
 // class component
 const ShopPage = ({ match, fetchCollectionsStart }) => {
@@ -17,15 +24,17 @@ const ShopPage = ({ match, fetchCollectionsStart }) => {
 
 	return (
 		<div className="shop-page">
-			<Route
-				exact
-				path={`${match.path}`}
-				component={CollectionOverviewContainer}
-			/>
-			<Route
-				path={`${match.path}/:collectionId`}
-				component={CollectionsPageContainer}
-			/>
+			<Suspense fallback={Spinner}>
+				<Route
+					exact
+					path={`${match.path}`}
+					component={CollectionOverviewContainer}
+				/>
+				<Route
+					path={`${match.path}/:collectionId`}
+					component={CollectionsPageContainer}
+				/>
+			</Suspense>
 		</div>
 	);
 };
